0f6116aa63274c81c2f853cc1085564ae4167716
perrypetiet <61829617+perrypetiet@users.noreply.github.com>
2020-11-12 22:35:53 +0100

    Added Sonarlint

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
-import java.util.concurrent.TransferQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class main {
 
 
     public static void main(String[] args) {
+
+        final Logger logger = Logger.getLogger("Logger");
 
         Scanner scanner = new Scanner(System.in);
 
@@ -12,30 +16,30 @@ public class main {
         double side2;
         double side3;
 
-        System.out.println("Welcome to the triangle type calculator. \n Enter side1: ");
+        logger.log(Level.INFO,"Welcome to the Triangle type calculator. \n Enter side1: ");
         side1 = scanner.nextInt();
-        System.out.println("Enter side 2: ");
+        logger.log(Level.INFO,"Enter side 2: ");
         side2 = scanner.nextInt();
-        System.out.println("Enter side 3: ");
+        logger.log(Level.INFO,"Enter side 3: ");
         side3 = scanner.nextInt();
 
 
-        triangle Triangle = new triangle(side1,side2,side3);
+        Triangle triangle = new Triangle(side1,side2,side3);
 
-        if (Triangle.getType() == 1){
-            System.out.println("The triangle is equilateral!");
+        if (triangle.getType() == 1){
+            logger.log(Level.INFO,"The Triangle is equilateral!");
         }
-        else if (Triangle.getType() == 2){
-            System.out.println("The triangle is right angle!");
+        else if (triangle.getType() == 2){
+            logger.log(Level.INFO,"The Triangle is right angle!");
         }
-        else if (Triangle.getType() == 3){
-            System.out.println("The triangle is scalene!");
+        else if (triangle.getType() == 3){
+            logger.log(Level.INFO,"The Triangle is scalene!");
         }
-        else if (Triangle.getType() == 4) {
-            System.out.println("The triangle is isosceles!");
+        else if (triangle.getType() == 4) {
+            logger.log(Level.INFO,"The Triangle is isosceles!");
         }
         else {
-            System.out.println("Unknown triangle!");
+            logger.log(Level.INFO,"Unknown Triangle!");
         }
 
 
